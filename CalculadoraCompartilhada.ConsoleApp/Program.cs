bb6b192c5fac1065d7e8288d843746cb9f6c6d5d
RuanMedTI <rurato2002@gmail.com>
2021-07-30 14:13:28 -0300

    Add project files.

---
+++ CalculadoraCompartilhada.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCompartilhada.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora 1.1");
+
+            Console.WriteLine("Digite 1 para somar");
+
+            Console.WriteLine("Digite 2 para subtrair");
+
+            Console.WriteLine("Digite 3 para multiplicar");
+
+            Console.WriteLine("Digite 4 para dividir");
+
+            Console.WriteLine("Digite 5 para apresentar as operacoes");
+
+            Console.WriteLine("Digite S para sair");
+        }
+    }
+}
